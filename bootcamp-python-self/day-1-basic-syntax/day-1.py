d9a08a699f1773508e1ca37bc610163f1321f68a
Yudi Yuswita Sunarto <yudiyuswita@gmail.com>
2025-04-27 17:16:02 +0700

    bootcamp self - day 1

---
+++ bootcamp-python-self/day-1-basic-syntax/day-1.py
@@ -0,0 +1,24 @@
+# Indentation
+if True:
+    print("This is inside the if block")
+else:
+    print("This is inside the else block")
+    
+    
+# For loop
+for i in range(2):
+    print(f"Iteration {i}")
+    
+# assignment vs comparison
+x = 5
+y = 10
+
+if x == y:
+    print("x is equal to y")
+else:
+    print("x is not equal to y")
+
+
+# not from bootcamp Customize for loops
+for i in range(5):
+    print(f"The actual value of i is {i} then + 5 so: {i+5}")
